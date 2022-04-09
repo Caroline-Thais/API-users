68fa32d579170f39bd6eeb446ce00fa326ac4e31
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-04-09 15:01:32 -0300

    cadastrando usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,47 @@
+const User = require("../models/User");
+
 class UserController{
 
     async index(req, res){};
 
     async create(req, res){
-        console.log(req.body);
-        res.send("Pegando o corpo da requisição.")
+
+        var{ email, name, password } = req.body;
+
+        if(email == undefined){
+            res.status(400);
+            res.json({err: "E-mail inválido."});
+            return
+        }
+
+        
+        if(emailExists){
+            res.status(406)
+            res.json({ err:"O email já está cadastrado"})
+            return
+        }
+
+        await User.new(email, password, name);
+        
+        res.status(200)
+        res.send("Tudo OK!")
+
+        if(name == undefined){
+            res.status(400);
+            res.json({err: "Nome de usuário inválido."})
+            return
+        }
+        res.status(200)
+        res.send("OK")
+
+        if(password == undefined){
+            res.status(400);
+            res.json({err: "Senha inválida"})
+            return
+        }
+
+        res.status(200)
+        res.send("OK")
     }
 }
 
